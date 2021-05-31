19b7dbf49cc910905611878d2aae873ca7bbb25e
Andr <amsh23453@gmail.com>
2021-05-31 20:38:03 +0700

    Sus

--- AI bot/Program.cs
+++ AI bot/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Speech.Synthesis;
+using System.IO;
 
 namespace AI_bot
 {
@@ -10,30 +12,50 @@ namespace AI_bot
     {
         static void Main(string[] args)
         {
-            string[] answers = { "Здравствуйте", "Хорошо", "Пока" };
-            string[] questions = { "Здравствуйте", "Как дела", "Пока" };
+            //string[] answers = { "Здравствуйте", "Хорошо", "Пока" };
+            //string[] questions = { "Здравствуйте", "Как дела?", "Пока" };
+            List<string> answers = File.ReadAllLines("answers.txt").ToList();
+            List<string> questions = File.ReadAllLines("questions.txt").ToList();
+            string idk = "I don't know, i'am too small you little idiot). But ok i remember";
 
+            //File.WriteAllLine("questions.txt", questions);
 
-            while (true)
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.SetOutputToDefaultAudioDevice();
+
+            string question = "";
+
+            do
             {
-                string question = Console.ReadLine(); // Задаем вопрос роботу - как дела
+                question = Console.ReadLine(); // Задаем вопрос роботу - как дела
 
-                for (int i = 0; i < questions.Length; i++)
+                for (int i = 0; i < questions.Count; i++)
                 {
                     if (questions[i] == question) // Если question(вопрос) == questions[i] => answers[i]
                     {
                         Console.WriteLine(answers[i]); // То ответит - answer[i]
+                        synth.Speak(answers[i]);
+
+                        break;
+                    }
+                    else if (i == questions.Count - 1)
+                    {
+                        Console.WriteLine("I don't know, i'am too small you little idiot). But ok i remember");
+                        synth.Speak(idk);
+
+                        questions.Add(question);
+                        answers.Add(Console.ReadLine());
+                        File.WriteAllLines("questions.txt", questions);
+                        File.WriteAllLines("answers.txt", answers);
                     }
                 }
 
-                if ("Пока" == question)
-                {
-                    break;
-                }
+                
             }
+            while ("bye" != question);
 
             Console.ReadKey();
         }
-
+ 
     }
 }
